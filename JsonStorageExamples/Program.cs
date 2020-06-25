d7f506a012fd1515c2df13c525e1901a2c6f5385
Don Lamont <5798888+dslamont@users.noreply.github.com>
2020-06-25 20:51:40 +0100

    Initilal commit

---
+++ JsonStorageExamples/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace JsonStorageExamples
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Create a sample object
+            StorageExampleClass exampleObj = new StorageExampleClass();
+            exampleObj.TextField = "Sample Text";
+            exampleObj.IntegerField = 23;
+
+            //Save the object to disk
+            string fileName = "SerializedObject.json";
+            using (FileStream fs = File.Create(fileName))
+            {
+                await JsonSerializer.SerializeAsync(fs, exampleObj);
+            }
+
+            //Create a new object from the saved Json
+            using (FileStream fs = File.OpenRead(fileName))
+            {
+                StorageExampleClass readinObj = await JsonSerializer.DeserializeAsync<StorageExampleClass>(fs);
+                Console.WriteLine($"Text Field = {readinObj.TextField}");
+                Console.WriteLine($"Integer Field = {readinObj.IntegerField}");
+
+                //Output
+                //Text Field = Sample Text
+                //Integer Field = 23
+            }
+        }
+    }
+}
---
+++ JsonStorageExamples/StorageExampleClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonStorageExamples
+{
+    class StorageExampleClass
+    {
+        public string TextField { get; set; }
+        public int IntegerField { get; set; } 
+    }
+}
